3bb3aa057ac478c44c99662ec224cf515dcb759d
Milo <camilo.iush@gmail.com>
2022-09-18 09:44:00 -0400

    Basline-constraint-Button-chain

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -9,6 +9,10 @@ import kotlinx.android.synthetic.main.activity_main.box_four_text
 import kotlinx.android.synthetic.main.activity_main.box_one_text
 import kotlinx.android.synthetic.main.activity_main.box_three_text
 import kotlinx.android.synthetic.main.activity_main.box_two_text
+import kotlinx.android.synthetic.main.activity_main.constraint_layout
+import kotlinx.android.synthetic.main.activity_main.green_button
+import kotlinx.android.synthetic.main.activity_main.red_button
+import kotlinx.android.synthetic.main.activity_main.yellow_button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,8 +23,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun setListeners() {
         val clickableViews: List<View> =
-            listOf(box_one_text, box_two_text, box_three_text,
-                box_four_text, box_five_text)
+                listOf(box_one_text, box_two_text, box_three_text,
+                        box_four_text, box_five_text, constraint_layout,
+                        red_button, green_button, yellow_button)
 
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
@@ -31,9 +36,12 @@ class MainActivity : AppCompatActivity() {
         when (view.id) {
             R.id.box_one_text -> view.setBackgroundColor(Color.GREEN)
             R.id.box_two_text -> view.setBackgroundColor(Color.BLACK)
-            R.id.box_three_text -> view.setBackgroundColor(Color.YELLOW)
+            R.id.box_three_text -> view.setBackgroundColor(Color.GREEN)
             R.id.box_four_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_five_text -> view.setBackgroundColor(Color.RED)
+            R.id.red_button -> box_three_text.setBackgroundColor(Color.RED)
+            R.id.yellow_button -> box_four_text.setBackgroundColor(Color.YELLOW)
+            R.id.green_button -> box_five_text.setBackgroundColor(Color.GREEN)
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
